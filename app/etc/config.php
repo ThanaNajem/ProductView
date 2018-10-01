15f0ff19e399585e80daf490564b9d2a0e2c6b4b
ThanaNajem <thana.najem@outlook.com>
2018-10-01 17:48:51 +0300

    add module file

--- app/etc/config.php
+++ app/etc/config.php
@@ -127,6 +127,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Mageplaza_Core' => 1,
         'Mageplaza_Smtp' => 1,
+        'Pulsestorm_HelloWorldMVVM' => 1,
         'Temando_Shipping' => 1,
         'Toptal_Blog' => 1,
         'Vertex_Tax' => 1
